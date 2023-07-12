2075bcceda475fe0fb72f44f6cc8aae84b005f65
Angie2119 <131821596+Angie2119@users.noreply.github.com>
2023-07-11 22:01:42 -0500

    Add files via upload

---
+++ pagprincipal.php
@@ -0,0 +1,66 @@
+<?php
+include("plantilla_cabecera.php");
+?>
+
+<!-- Main Sidebar Container -->
+<?php
+include("plantilla_menu.php");
+?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+
+        <div class="col-sm-6">
+          <h1>
+            Bienvendido, <?php echo  $_SESSION["USUARIO"]; ?>
+          </h1>
+        </div>
+
+
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active">Dashboard</li>
+          </ol>
+        </div>
+      </div>
+    </div><!-- /.container-fluid -->
+  </section>
+
+  <!-- Main content -->
+  <section class="content">
+
+    <!-- Default box -->
+    <div class="card">
+      <div class="card-header">
+        <h3 class="card-title">Usuario</h3>
+
+        <div class="card-tools">
+          <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+            <i class="fas fa-minus"></i>
+          </button>
+          <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+            <i class="fas fa-times"></i>
+          </button>
+        </div>
+      </div>
+      <div class="card-body">
+        Accede a tu perfil
+      </div>
+
+      <!-- /.card-footer-->
+    </div>
+    <!-- /.card -->
+
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<?php
+include("plantilla_pie.php");
+?>
